009b06e6c09fa01290b99f65a0ac18fafd4a1e0e
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2021-09-21 17:37:22 +0400

    lowerBound, upperBound and occurences for sorted array.

--- src/search/BinarySearch.h
+++ src/search/BinarySearch.h
@@ -3,35 +3,57 @@
 #include <vector>
 
 namespace alg {
+namespace {
 
-template <typename T>
-bool binarySearch(const std::vector<T>& v, T key, size_t low, size_t high)
-{
-  if (low > high) return false;
-  if (high > v.size()) return false;
+template <typename T, typename Cmp>
+int64_t binarySearch(const std::vector<T>& v, T key, int64_t low, int64_t high,
+                     const Cmp& cmp) {
+  while (low < high) {
+    auto mid = (high + low) / 2;
 
-  auto mid = (low + high) / 2;
-
-  if (v[mid] > key) {
-    return binarySearch(v, key, low, mid - 1);
-  } else if (v[mid] < key) {
-    return binarySearch(v, key, mid + 1, high);
+    if (cmp(key, v[mid])) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
   }
 
-  return true;
+  return low;
 }
+
+}  // namespace
 
 /**
  * @brief  Implementation of classic binary search algorithm
  *
- * @param v An input array
+ * @param v An input array sorted in a ascending order
  * @param key The key to search inside an array
  *
  * @return  Return true if the key is found, otherwise false
  */
 template <typename T>
 bool binarySearch(const std::vector<T>& v, T key) {
-  return binarySearch(v, key, 0, v.size() - 1);
+  const auto i = binarySearch(v, key, 0, v.size() - 1, std::less_equal<T>());
+
+  return i >= 0 && i < static_cast<int64_t>(v.size()) && v[i] == key;
+}
+
+
+template <typename T>
+int64_t lowerBound(const std::vector<T>& v, T key) {
+  return binarySearch(v, key, 0, v.size(), std::less_equal<T>());
+}
+
+
+template <typename T>
+int64_t upperBound(const std::vector<T>& v, T key) {
+  return binarySearch(v, key, 0, v.size(), std::less<T>());
+}
+
+
+template <typename T>
+int64_t occurences(const std::vector<T>& v, T key) {
+  return upperBound(v, key) - lowerBound(v, key);
 }
 
 }  // namespace alg
--- test/search/BinarySearchTest.cpp
+++ test/search/BinarySearchTest.cpp
@@ -18,3 +18,45 @@ TEST(BinarySearchTests, BinarySearch) {
     EXPECT_TRUE(binarySearch(a, i)) << i << " was not found";
   }
 }
+
+
+TEST(BinarySearchTests, LowerBound) {
+  ArrayInts a{1, 4, 4, 5};
+
+  EXPECT_EQ(lowerBound(a, 0), 0);
+  EXPECT_EQ(lowerBound(a, 1), 0);
+  EXPECT_EQ(lowerBound(a, 4), 1);
+  EXPECT_EQ(lowerBound(a, 5), 3);
+
+  a = {4, 4, 4, 4, 5};
+
+  EXPECT_EQ(lowerBound(a, 1), 0);
+  EXPECT_EQ(lowerBound(a, 4), 0);
+  EXPECT_EQ(lowerBound(a, 5), 4);
+  EXPECT_EQ(lowerBound(a, 6), 5);
+}
+
+
+TEST(BinarySearchTests, UpperBound) {
+  ArrayInts a{1, 4, 4, 5};
+
+  EXPECT_EQ(upperBound(a, 0), 0);
+  EXPECT_EQ(upperBound(a, 1), 1);
+  EXPECT_EQ(upperBound(a, 4), 3);
+  EXPECT_EQ(upperBound(a, 5), 4);
+
+  a = {4, 4, 4, 4, 5};
+
+  EXPECT_EQ(upperBound(a, 1), 0);
+  EXPECT_EQ(upperBound(a, 4), 4);
+  EXPECT_EQ(upperBound(a, 5), 5);
+  EXPECT_EQ(upperBound(a, 6), 5);
+}
+
+TEST(BinarySearchTests, Occurences) {
+  ArrayInts a{1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+
+  for (int i = 1; i <= 4; ++i) {
+    EXPECT_EQ(occurences(a, i), i);
+  }
+}

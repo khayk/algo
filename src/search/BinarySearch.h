e770715b9a4893cb0502432d066d12ac96277984
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2021-09-21 14:59:39 +0400

    Binary search implementation

--- src/search/BinarySearch.h
+++ src/search/BinarySearch.h
@@ -10,11 +10,25 @@ bool binarySearch(const std::vector<T>& v, T key, size_t low, size_t high)
   if (low > high) return false;
   if (high > v.size()) return false;
 
-  std::ignore = key;
+  auto mid = (low + high) / 2;
 
-  return false;
+  if (v[mid] > key) {
+    return binarySearch(v, key, low, mid - 1);
+  } else if (v[mid] < key) {
+    return binarySearch(v, key, mid + 1, high);
+  }
+
+  return true;
 }
 
+/**
+ * @brief  Implementation of classic binary search algorithm
+ *
+ * @param v An input array
+ * @param key The key to search inside an array
+ *
+ * @return  Return true if the key is found, otherwise false
+ */
 template <typename T>
 bool binarySearch(const std::vector<T>& v, T key) {
   return binarySearch(v, key, 0, v.size() - 1);

e59894459e4cd753c278e0c6f0c7c96d1ef76da1
Hayk Karapetyan <karapetyan.hayk@gmail.com>
2022-02-27 21:27:25 +0400

    Maximum subarray sum

--- src/array/Problems.h
+++ src/array/Problems.h
@@ -1,9 +1,32 @@
 #pragma once
 
-#include <limits>
-#include <tuple>
 #include <vector>
+#include <type_traits>
 
 namespace alg {
+
+/**
+ * @brief  Given an array of n numbers, the task is to calculate the maximum
+ *         subarray sum, i.e., the largest possible sum of a sequence of
+ *         consecutive values in the array
+ *
+ * @param a  An input array
+ *
+ * @return  The resulting sum
+ */
+
+template <typename T>
+int maximumSubarraySum(const std::vector<T>& a) {
+  static_assert(std::is_integral_v<T>, "T must integer type");
+
+  T best{}, sum{};
+
+  for (const auto& i: a) {
+    sum = std::max(i, sum + i);
+    best = std::max(best, sum);
+  }
+
+  return best;
+}
 
 }  // namespace alg
--- test/array/ProblemsTest.cpp
+++ test/array/ProblemsTest.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 #include "array/Problems.h"
 
-TEST(ArrayProblemsTests, RenameMe) {
+using namespace alg;
+
+TEST(ArrayProblemsTests, MaximumSubarraySum) {
+  std::vector<int> a = {-1, 2, 4, -3, 5, 2, -5, 2};
+  EXPECT_EQ(maximumSubarraySum(a), 10);
+
+  a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  EXPECT_EQ(maximumSubarraySum(a), 6);
+
+  // Edge cases
+  EXPECT_EQ({}, 0);
+  EXPECT_EQ({7}, 7);
+  EXPECT_EQ({-1}, -1);
 }
